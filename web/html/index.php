f6142ed93a466359c49b17fd863f01142fc600df
sovap <peter.sova@cgi.com>
2024-01-23 15:01:08 +0100

    first commit

---
+++ web/html/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Learning By Doing</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+    <!-- main  -->
+    <div class="container p-3">
+
+      <div class="container-fluid py-4 px-0">
+        <h1 class="display-5 fw-bold">Home</h1>
+      </div>
+
+      <div class="container-fluid py-4 px-0">
+        <a class="btn btn-primary" href="./practice">Go forward to Practice</a>
+      </div>
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
